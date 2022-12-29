069e35efa2f7861cdce949984fb6e2bd1ed8e85e
Kuyho Chung <kchung1995@gmail.com>
2022-12-29 23:18:56 +0900

    Initial commit

---
+++ src/main/kotlin/com/tistory/katfun/mapleanalysis/MapleAnalysisApplication.kt
@@ -0,0 +1,11 @@
+package com.tistory.katfun.mapleanalysis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MapleAnalysisApplication
+
+fun main(args: Array<String>) {
+    runApplication<MapleAnalysisApplication>(*args)
+}
---
+++ src/test/kotlin/com/tistory/katfun/mapleanalysis/MapleAnalysisApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tistory.katfun.mapleanalysis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MapleAnalysisApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
