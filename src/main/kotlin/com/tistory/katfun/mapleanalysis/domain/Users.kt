e15cc40b51120f4dc5371539bf7808b52c7cd34b
Kuyho Chung <kchung1995@gmail.com>
2022-12-29 23:19:25 +0900

    Users entity 생성

---
+++ src/main/kotlin/com/tistory/katfun/mapleanalysis/domain/Users.kt
@@ -0,0 +1,16 @@
+package com.tistory.katfun.mapleanalysis.domain
+
+import java.time.LocalDateTime
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class Users (
+    var token: String,
+    var createdAt: LocalDateTime = LocalDateTime.now(),
+    var editedAt: LocalDateTime = LocalDateTime.now(),
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long
+)
+
