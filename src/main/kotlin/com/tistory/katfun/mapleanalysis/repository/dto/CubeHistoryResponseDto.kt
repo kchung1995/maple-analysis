623a0463660c8addf292eea2741a5a5303c3cf79
Kuyho Chung <kchung1995@gmail.com>
2023-01-16 01:00:58 +0900

    Dto camelcase, snakecase 대응 추가

--- src/main/kotlin/com/tistory/katfun/mapleanalysis/repository/dto/CubeHistoryResponseDto.kt
+++ src/main/kotlin/com/tistory/katfun/mapleanalysis/repository/dto/CubeHistoryResponseDto.kt
@@ -1,5 +1,9 @@
 package com.tistory.katfun.mapleanalysis.repository.dto
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class CubeHistoryResponseDto(
     val count: Int,
     val cubeHistories: Array<CubeHistoryResponseDto>,
